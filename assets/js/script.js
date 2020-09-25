8352236553cb7f3d9bbffd43e6ba7f8be74ea8b5
Angel <angelarciniaga@gmail.com>
2020-09-24 21:14:37 -0300

    Desafio funcional, hay que limpiar un poco el codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,29 +74,41 @@ let validando = (event) => {
     event.preventDefault();
     let nombre = document.getElementById('propietario').value;
     let direccion = document.getElementById('direccion').value;
-    let telefono = parseInt(document.getAnimations('telefono').value);
+    let telefono = document.getElementById('telefono').value;
     let nombreAnimal = document.getElementById('nombreMascota').value;
     let tipo = document.getElementById('tipo').value;
     let enfermedad = document.getElementById('enfermedad').value;
 
     let pro1 = new Propietario(nombre, direccion, telefono);
-    let ani1 = new Animal(nombre, direccion, telefono, nombreAnimal, tipo, enfermedad);
+    let ani1 = new Perro(nombre, direccion, telefono, nombreAnimal, tipo, enfermedad) || new Gato(nombre, direccion, telefono, nombreAnimal, tipo, enfermedad) || new Conejo(nombre, direccion, telefono, nombreAnimal, tipo, enfermedad);
+    switch(ani1 == 'Perro') {
+        case 'Perro':
+            ani1 = new Perro(nombre, direccion, telefono, nombreAnimal, tipo, enfermedad);
+            break;
+        case 'Gato':
+            ani1 = new Gato(nombre, direccion, telefono, nombreAnimal, tipo, enfermedad);
+            break;
+        case 'Conejo':
+            ani1 = new Conejo(nombre, direccion, telefono, nombreAnimal, tipo, enfermedad);
+            break;
+    }
 
 
 
     document.getElementById('resultado').innerHTML =    `<ul>
                                                             <li>El nombre del dueño es: ${pro1.datosPropietario()}</li>
-                                                            <li>El nombre del animal es: ${ani1.datosAnimal()} y la enfermedad es: ${ani1.otraEnfermedad}</li>
+                                                            <li>El nombre del animal es: ${ani1.datosAnimal()} y la enfermedad es: ${ani1.enfermedad}</li>
                                                         </ul>`;
 
                                                         console.log(Propietario.telefono);
+                                                        console.log(ani1);
 
 }
 
 formulario.addEventListener('submit', validando)
 
 /* let pro1 = new Propietario('Angel', 'Santiago Centro', 931231795);
-let ani1 = new Animal('Angel', 'Santiago Centro', '931231795', 'Kira', 'Perro', 'otitis');
+let ani1 = new Perro('Angel', 'Santiago Centro', '931231795', 'Kira', 'Perro', 'otitis');
 
 console.log(pro1.telefono);
 console.log(ani1.enfermedad); */
